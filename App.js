4d5a43e2f8c156e6cdf77ece61b9f84af7535a5c
Samara Silvia <100232025+SamaraSilvia81@users.noreply.github.com>
2023-05-11 18:04:36 -0300

    Created API, Screen MarvelScreen and Component CardHeroes

--- App.js
+++ App.js
@@ -1,90 +1,87 @@
 import React from 'react';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators  } from '@react-navigation/stack';
-import Hero from './screens/HeroScreen';
-import Home from './screens/HomeScreen';
-import Marvel from './screens/MarvelScreen';
-import MarvelHeroes from './screens/Marvel/HeroesScreen';
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+import Hero from './src/screens/HeroScreen';
+import Home from './src/screens/HomeScreen';
+import Marvel from './src/screens/MarvelScreen';
+import MarvelHeroes from './src/screens/MarvelHeroes';
+
+const queryClient = new QueryClient();
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Hero"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: '#ffffff',
-          },
-          cardStyleInterpolator:
-            CardStyleInterpolators.forHorizontalIOS,
-          headerLeftContainerStyle: {
-              paddingLeft: 20,
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Hero"
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#ffffff',
             },
-        }}>
-        <Stack.Screen
-          name="Hero"
-          component={Hero}
-          options={{ 
-            headerShown: false,
-            headerLeft: () => (
-              <TouchableOpacity onPress={() => {}}>
-                <Image
-                  source={require('./images/arrow.png')}
-                  style={{ width: 24, height: 24 }}
-                />
-              </TouchableOpacity>
-            )
-          }}
-        />
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{ 
-            headerShown: false,
-            headerLeft: () => (
-              <TouchableOpacity onPress={() => {}}>
-                <Image
-                  source={require('./images/arrow.png')}
-                  style={{ width: 24, height: 24 }}
-                />
-              </TouchableOpacity>
-            )
-          }}
-        />
-        <Stack.Screen
-          name="Marvel"
-          component={Marvel}
-          options={{ 
-            headerShown: false,
-            headerLeft: () => (
-              <TouchableOpacity onPress={() => {}}>
-                <Image
-                  source={require('./images/arrow.png')}
-                  style={{ width: 24, height: 24 }}
-                />
-              </TouchableOpacity>
-            )
-          }}
-        />
-        <Stack.Screen
-          name="MarvelHeroes"
-          component={MarvelHeroes}
-          options={{ 
-            headerShown: false,
-            headerLeft: () => (
-              <TouchableOpacity onPress={() => {}}>
-                <Image
-                  source={require('./images/arrow.png')}
-                  style={{ width: 24, height: 24 }}
-                />
-              </TouchableOpacity>
-            )
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+            cardStyleInterpolator:
+              CardStyleInterpolators.forHorizontalIOS,
+            headerLeftContainerStyle: {
+                paddingLeft: 20,
+              },
+          }}>
+          <Stack.Screen
+            name="Hero"
+            component={Hero}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{
+              headerStyle: {
+                backgroundColor: '#23232E',
+                height: 80,
+              },
+              headerTintColor: 'white',
+              headerTitleAlign: 'center',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+          />
+          <Stack.Screen
+            name="Marvel"
+            component={Marvel}
+            options={{
+              headerStyle: {
+                backgroundColor: '#23232E',
+                height: 80,
+              },
+              headerTintColor: 'white',
+              headerTitleAlign: 'center',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+          />
+          <Stack.Screen
+            name="MarvelHeroes"
+            component={MarvelHeroes}
+            options={{
+              headerStyle: {
+                backgroundColor: '#23232E',
+                height: 80,
+              },
+              headerTintColor: 'white',
+              headerTitleAlign: 'center',
+              headerTitleStyle: {
+                fontWeight: 'bold',
+              },
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
--- README.md
+++ README.md
@@ -1,13 +1,5 @@
-# World Geek
+# Project Tasks - React Native
+![Badge Concluído](http://img.shields.io/static/v1?label=STATUS&message=CONCLUÍDO&color=5218e7&style=for-the-badge)
 
-> O projeto é voltado para expor um universo nerd, referente as indústrias Marvel Comics e DC Comics. 
-
-## O Projeto World's Geek
-
-Neste projeto, conterá uma tela inicial com a logo e após ser clicada ser direcionada para uma página onde o usuário poderá escolher sobre quais heróis ou vilões de qual indústria quer vê.
-
-Este apptem como intuito diviertir os amantes dos universos dos heróis e vilões com características, detalhes do avatar e muito mais, tudo isso por meio de uma interface simples e intuitiva.
-
-## Versão 01
-
-Essa é a primeira versão do nosso projeto
+## Deploy Tasks
+![](https://github.com/SamaraSilvia81/AppTestReactNative/assets/100232025/188d2ef5-d9fd-4104-9e03-a6d7c524de44)
